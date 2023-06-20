9eedb1a9c0a579eccc887aa3afd24a8ff19a7655
simra4693 <137105411+simra4693@users.noreply.github.com>
2023-06-20 12:32:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,6 @@ import streamlit
 streamlit.title('Hello Everyone')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Paratha')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
