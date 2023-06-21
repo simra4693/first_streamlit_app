e44fecc9c4600c705245e6f8518b012ae620044b
simra4693 <137105411+simra4693@users.noreply.github.com>
2023-06-21 23:34:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,8 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# normalize the json version
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# give output
+streamlit.dataframe(fruityvice_normalized)
+
