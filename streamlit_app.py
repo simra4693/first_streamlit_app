ab5af0999fcc3b80c45bbd09bfaccbb30851865c
simra4693 <137105411+simra4693@users.noreply.github.com>
2023-06-21 23:30:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,7 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.dataframe(my_fruit_list)
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['1','2'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.set_index),['Avocado','Apple'])
 
 
 #New Section to import API fruitvice
